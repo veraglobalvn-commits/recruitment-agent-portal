59dd9d00bb2a08e67802b7dfe4055d5f89cd5934
veraglobalvn-commits <veraglobal.vn@gmail.com>
2026-04-09 05:34:46 +0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Thêm ' || '' ' để lỡ không có key thì tạm để trống, tránh lỗi lúc Vercel đang đóng gói
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Nếu không có key (lúc đóng gói) thì trả về null, không báo lỗi
+export const supabase = supabaseUrl && supabaseAnonKey ? createClient(supabaseUrl, supabaseAnonKey) : null
